09338a1b80b535b87bbfdd88bdc6e23cd78e4eb3
Genta Ito <itogenta.toge@gmail.com>
2019-09-08 11:12:40 +0900

    initial commit

---
+++ crud_front/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MainContainer from "./Components/MainContainer";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <MainContainer />
+    </div>
+  );
+};
+
+export default App;
